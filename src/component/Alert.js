011de03c2d9046731b789c90358c96f28a856c13
shivamdevloper123 <shivam71066@gmail.com>
2024-08-21 12:02:50 +0530

    video-63 completed

--- src/component/Alert.js
+++ src/component/Alert.js
@@ -1,19 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function Alert(props) {
-  const [visible, setVisible] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVisible(false);
-    }, 2000); // Alert will disappear after 2000 milliseconds (2 seconds)
-
-    return () => clearTimeout(timer); // Cleanup the timer if the component unmounts
-  }, []);
-
-  if (!visible) {
-    return null; // Don't render anything if the alert is not visible
-  }
 
   return (
     <div className="alert alert-warning" role="alert">
--- src/component/Notes.js
+++ src/component/Notes.js
@@ -1,24 +1,25 @@
 import React, { useContext } from 'react';
 import noteContext from '../context/note/noteContext';
 import Noteitem from './Noteitem';
-
+import AddNote from './AddNote';
 function Notes() {
     const context = useContext(noteContext);
-    const { notes } = context;
+    const {notes,addNote} = context;
 
     return (
+        <>
+        <AddNote/>
         <div className="container">
             <div className="row" >
             <h2>Your Notes</h2>
-           
-                {notes.map((note) => {
-             
-                    return <Noteitem key={note._id} note={note} />
+              {notes.map((note) => {
+             return <Noteitem key={note._id} note={note} />
                    
                 })}
            
             </div>
         </div>
+        </>
     );
 }
 
