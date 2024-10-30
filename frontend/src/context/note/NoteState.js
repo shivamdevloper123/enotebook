6a87dc6399e89ed4063db6014758eacaf0734189
shivamdevloper123 <shivam71066@gmail.com>
2024-10-30 23:37:21 +0530

    like old react course

--- frontend/src/context/note/NoteState.js
+++ frontend/src/context/note/NoteState.js
@@ -2,7 +2,7 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = `https://enotebook-backend-eosin.vercel.app`
+  const host = `http://localhost:5000`
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
